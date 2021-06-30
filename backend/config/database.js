3b9ba8144bd5830795c87d5b2d910d5fcaa1d399
Max <e13lun4@yandex.ru>
2021-06-30 17:54:09 +0300

    project is started

---
+++ backend/config/database.js
@@ -0,0 +1,20 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST', 'clusterlmc.hknuy.mongodb.net'),
+        srv: env.bool('DATABASE_SRV', true),
+        port: env.int('DATABASE_PORT', 27017),
+        database: env('DATABASE_NAME', 'backend_db'),
+        username: env('DATABASE_USERNAME', 'admin'),
+        password: env('DATABASE_PASSWORD', 'Password123'),
+      },
+      options: {
+        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'adf68bc9-3f01-4060-b0d0-317ccea013d5'
+};
